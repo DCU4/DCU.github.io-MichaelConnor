9277c1e0e06f29cdfc1e5c102efed5635fbaf656
Dylan Connor <DylanJConnor4@gmail.com>
2018-03-15 17:28:13 +0000

    increase fadeIn for .container

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
 
 	$(window).on('load', function() {
 		$('.loader').fadeIn(500).delay(500).fadeOut(3000);
-		$('.container').delay(4000).fadeIn(1000);
+		$('.container').delay(4000).fadeIn(1500);
 
 })
 
