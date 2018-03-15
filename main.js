b83d6910891d3c80113dfc6fd14fd65cb2c9a748
DCU4 <DylanJConnor4@gmail.com>
2018-03-15 17:00:55 +0000

    Save my work

---
+++ main.js
@@ -0,0 +1,20 @@
+
+$(document).ready(function(){
+
+	$('html').fadeIn(1000);
+
+
+
+	$(window).on('load', function() {
+		$('.loader').fadeIn(500).delay(500).fadeOut(3000);
+		$('.container').delay(4000).fadeIn(1000);
+
+})
+
+
+	//project pages
+	$('#project-container').fadeIn(2500);
+
+	
+});
+
