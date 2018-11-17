a1e19e358970482f0295e14970546f5a01a225b4
DCU4 <DylanJConnor4@gmail.com>
2018-11-17 16:37:56 -0500

    update repo

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
 
 	$(window).on('load', function() {
 		$('.loader').fadeIn(500).delay(500).fadeOut(3000);
-		$('.container').delay(4000).fadeIn(1000);
+		$('.container').delay(4000).fadeIn(2000);
 
 })
 
@@ -16,5 +16,36 @@ $(document).ready(function(){
 	$('#project-container').fadeIn(2500);
 
 	
+
+
+	$(window).on('scroll', function(){
+		if ($(window).scrollTop() < 799) {
+		$(".education").animate({
+ 		opacity: '1',
+ 		right:'0px'
+    	},1000);
+
+	} else if ($(window).scrollTop() < 2300)  {
+		$(window).on('scroll', function(){
+		$(".experience").animate({
+ 		opacity: '1',
+ 		left:'0'	
+    	},1000);
+	});
+
+	} else {
+		$(window).on('scroll', function(){
+		$(".skills").animate({
+ 		opacity: '1',
+ 		right:'0px'
+    	},1000);
+	});
+	}
+
+});
+
+
+
+
 });
 
